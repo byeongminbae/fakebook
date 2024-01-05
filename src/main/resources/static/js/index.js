8847e4390fdc3401f6ed6021a49098982d818826
Byeongmin Bae <151404574+byeongminbae@users.noreply.github.com>
2024-01-05 17:28:25 +0900

    Frontend RefreshToken test completed

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -18,63 +18,83 @@ function getCookies() {
 }
 
 function setCookie(cookieName, cookieValue, expiredAt) {
-    const date = new Date(expiredAt).toLocaleString();
-    document.cookie = cookieName + "=" + cookieValue + ";" + `expiredAt=${date};` + ";path=/";
+    document.cookie = cookieName + "=" + cookieValue + ";" + `expires=${new Date(expiredAt).toString()};` + ";path=/";
 }
 
 
 function getToken(tokenType) {
-    return getCookies().find(cookie => cookie.startsWith(tokenType + '='));
-}
-
-function checkTokens() {
-    if (window.location.pathname != `/${Site.SIGNIN}`) {
-        setInterval(() => {
-            const accessToken = getToken(TokenType.ACCESS);
-            const refreshToken = getToken(TokenType.REFRESH);
-
-            if (!accessToken) {
-                if (!refreshToken) {
-                    redirect(Site.SIGNIN)
-                }
-                $.ajax({
-                    method: 'POST',
-                    url: '/auth/token/renew',
-                    contentType: "application/json",
-                    dataType: "json",
-                    data: JSON.stringify({ refreshToken: refreshToken }),
-                    success: function (response) {
-                        if (response.success) {
-                            setCookie("accessToken", response.data.accessTokenResponseDto.token, response.data.accessTokenResponseDto.expiredAt);
-                            console.log(response)
-                            return;
-                        }
-                        redirect(Site.SIGNIN)
-                    }
-                });
-            }
-
-        }, 5000);
+    const cookie = getCookies().find(cookie => cookie.includes(`${tokenType}=`));
+    if (cookie) {
+        return cookie.split("=")[1]
     }
 }
 
-function requestSignIn(id, password) {
-    $.ajax({
-        type: "POST",
-        url: "http://127.0.0.1:8080/auth/token/sign-in",
-        contentType: "application/json",
-        dataType: "json",
-        data: JSON.stringify({ signId: id, signPassword: password }),
-        success: function (response) {
-            if (response.success) {
-                setCookie("accessToken", response.data.accessTokenResponseDto.token, response.data.accessTokenResponseDto.expiredAt);
-                setCookie("refreshToken", response.data.refreshTokenResponseDto.token, response.data.refreshTokenResponseDto.expiredAt)
-                redirect(Site.INDEX)
-                return;
+function asyncAjax({ method, url, data }) {
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            method: method,
+            url: url,
+            contentType: "application/json",
+            dataType: "json",
+            data: data,
+            beforeSend: function () {
+            },
+            success: function (data) {
+                resolve(data)
+            },
+            error: function (err) {
+                reject(err)
             }
-            alert("ID or Password incorrect");
-        },
+        });
     });
 }
 
+async function requestAccessToken() {
+    try {
+        const refreshToken = getToken(TokenType.REFRESH);
+        if (!refreshToken) {
+            redirect(Site.SIGNIN)
+            return;
+        }
+        const response = await asyncAjax({
+            method: "POST",
+            url: '/auth/token/renew',
+            data: JSON.stringify({ refreshToken: refreshToken }),
+        })
+        if (response.success) {
+            setCookie("accessToken", response.data.token, response.data.expiredAt);
+            return response;
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
 
+async function requestSignIn(id, password) {
+    const response = await asyncAjax({
+        method: "POST",
+        url: "/auth/token/sign-in",
+        data: JSON.stringify({ signId: id, signPassword: password })
+    })
+
+    if (response.success) {
+        setCookie("accessToken", response.data.accessTokenResponseDto.token, response.data.accessTokenResponseDto.expiredAt);
+        setCookie("refreshToken", response.data.refreshTokenResponseDto.token, response.data.refreshTokenResponseDto.expiredAt)
+        redirect(Site.INDEX)
+        return response;
+    }
+    alert("ID or Password incorrect");
+}
+
+async function updateAccessToken() {
+    if (window.location.pathname != `/${Site.SIGNIN}`) {
+        const refreshToken = getToken(TokenType.REFRESH);
+        if (!refreshToken) {
+            redirect(Site.SIGNIN)
+        }
+        requestAccessToken();
+        setInterval(() => requestAccessToken(), 5000);
+    }
+}
+
+window.onload = updateAccessToken();
